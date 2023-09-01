cc99744274c4c0df239c656e4405effcec8db83b
MOHAMMAD NOUSHAD <116102583+noushadkas@users.noreply.github.com>
2023-09-01 18:34:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-function calculateMinCost() {
-  //your code here
-  
-  
-  
-}  
+// Min-heap implementation using an array
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  // ... MinHeap methods (insert, extractMin, isEmpty, bubbleUp, sinkDown) ...
+
+}
+
+function findMinimumCostRopes(ropes) {
+  // Initialize a min-heap
+  const minHeap = new MinHeap();
+
+  // Insert all the ropes into the min-heap
+  for (const rope of ropes) {
+    minHeap.insert(rope);
+  }
+
+  let totalCost = 0;
+
+  // Merge ropes until there's only one left in the heap
+  while (!minHeap.isEmpty() && minHeap.heap.length > 1) {
+    const rope1 = minHeap.extractMin();
+    const rope2 = minHeap.extractMin();
+    const newRope = rope1 + rope2;
+    totalCost += newRope;
+    minHeap.insert(newRope);
+  }
+
+  return totalCost;
+}
+
+// Handle form submission
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("ropeForm");
+  const resultDiv = document.getElementById("result");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.getElementById("ropeInput").value;
+    const ropes = input.split(",").map((str) => parseInt(str.trim(), 10));
+    const minimumCost = findMinimumCostRopes(ropes);
+    resultDiv.textContent = `Minimum Cost: ${minimumCost}`;
+  });
+});
